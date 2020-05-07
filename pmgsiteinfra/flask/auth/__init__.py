216f6e10a23ca6b96eeec20dcb10770053cab26c
pete <possemeeg@gmail.com>
2020-05-07 13:46:11 +0100

    Logon with own page

--- pmgsiteinfra/flask/auth/__init__.py
+++ pmgsiteinfra/flask/auth/__init__.py
@@ -1,7 +1,7 @@
 from authlib.oidc.core import CodeIDToken
 from authlib.jose import jwt
 from authlib.integrations.flask_client import OAuth
-from flask import redirect, url_for, session, g
+from flask import redirect, url_for, session, g, request
 from logging import getLogger
 import requests
 from functools import wraps
@@ -34,12 +34,14 @@ class AuthServerCallError(Exception):
 class AuthApp(OAuth):
     def __init__(self, app, logon_endpoint):
         super(AuthApp, self).__init__(app) #TODO, cache=self.cache)
-        self.register('govsieauth')
         app.before_request(self._before_request)
         app.after_request(self._after_request)
         self.id_token_certs = None
         self.logon_endpoint = logon_endpoint
         self.logon_hooks = []
+
+    def register(self, **reg_args):
+        super(AuthApp, self).register('govsieauth', **reg_args)
 
     def _before_request(self):
         pass
@@ -50,10 +52,19 @@ class AuthApp(OAuth):
     def register_logon_hook(self, call):
         self.logon_hooks.append(call)
 
-    def logon_page(self, redirect_endpoint):
-        
-        
-        #params = dict(scope='openid+profile', access_type='offline', response_type='code')
+    #def logon_page_endpoing(self, redirect_endpoint):
+    #    return self.govsieauth.authorize_redirect(redirect_url)#, **params)
+    @property
+    def server_metadata(self):
+        self.govsieauth._load_server_metadata()
+        return self.govsieauth.server_metadata
+
+    def logon_page(self, redirect_endpoint, render = None):
+        args = {k:v for k, v in request.args.items() if k in ['client_id', 'redirect_uri', 'state']}
+        if args:
+            if not render:
+                raise RuntimeError('To re-enter logon page a render function is required.')
+            return render(auth_url=self.server_metadata['authorization_endpoint'], **args)
     
         redirect_url = url_for(redirect_endpoint, _external=True)
     
@@ -61,21 +72,23 @@ class AuthApp(OAuth):
     
     def get_oauth2_certs(self):
         if not self.id_token_certs:
-            self.id_token_certs = AuthServerCallError.wrap_call(self.govsieauth.get, self.govsieauth.server_metadata['jwks_uri'])
+            self.id_token_certs = AuthServerCallError.wrap_call(self.govsieauth.get, self.server_metadata['jwks_uri'])
         return self.id_token_certs
     
     
-    def authorise_flow(self, redirect_endpoint):
+    def authorise_flow(self, default_redirect_endpoint):
         access_token = self.govsieauth.authorize_access_token()
     
         key = self.get_oauth2_certs()
         id_token = jwt.decode(access_token['id_token'], key, claims_cls=CodeIDToken)
         id_token.validate()
 
-        user_info = AuthServerCallError.wrap_call(self.govsieauth.get, self.govsieauth.server_metadata['userinfo_endpoint'])
+        user_info = AuthServerCallError.wrap_call(self.govsieauth.get, self.server_metadata['userinfo_endpoint'])
         self._create_session(access_token=access_token, id_token=id_token, user_info=user_info)
+
+        redirect_endpoint = session['requested_page'] if 'requested_page' in session else url_for(default_redirect_endpoint)
     
-        return redirect(url_for(redirect_endpoint))
+        return redirect(redirect_endpoint)
 
     def user_id(self):
         return id_token('sub')
@@ -97,7 +110,7 @@ class AuthApp(OAuth):
         @wraps(f)
         def decorated(*args, **kwargs):
             if 'auth_id_token' not in session:
-                g.requested_page = f
+                session['requested_page'] = request.url
                 return redirect(url_for(self.logon_endpoint))
             return f(*args, **kwargs)
     
